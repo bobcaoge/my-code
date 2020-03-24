93065eefeefb6e9da116182266ca6b2977a93a96
bobcaoge <378082326@qq.com>
2020-03-24 20:24:29 +0800

    练习‘

--- C++/temp/temp/main.cpp
+++ C++/temp/temp/main.cpp
@@ -1,11 +1,27 @@
 #include<iostream>
 #include "LinkStack.h"
 using namespace std;
+class Base {
+public:
+	static int num;
+	Base() {
+		num++;
+	}
+	void show_num() {
+		cout << num << endl;
+	}
+};
+int Base::num = 0;
+class SubBase: public Base {
 
+};
 
 int main()
 {
-	LinkStack <string> lstack;
+	Base b;
+	b.show_num();
+	SubBase sub;
+	sub.show_num();
 
 
 	getchar();
