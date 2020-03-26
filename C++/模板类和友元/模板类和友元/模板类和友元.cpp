665ed89dcae927e6306b0106116e805500f4909d
bobcaoge <378082326@qq.com>
2020-03-26 11:18:27 +0800

    类模板和友元

---
+++ C++/模板类和友元/模板类和友元/模板类和友元.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+template<typename T> class MyClass;
+template<typename T> void f_run(MyClass<T> a);
+template<typename T> class F_class;
+
+
+template <typename T>
+class MyClass
+{
+public:
+	MyClass(T a) :t(a){};
+	void run(){
+		cout << "run" << endl;
+	}
+
+	friend void friend_run(MyClass<T> a){
+		cout << "friend_run" << endl;
+		cout << a.data << endl;
+	}
+	friend void f_run<T>(MyClass<T> a);
+	friend class F_class<T>;
+private:
+	int data=3;
+	T t;
+};
+template<typename T>
+void f_run(MyClass<T> a){
+		cout << "f_run" << endl;
+		cout << a.data << endl;
+}
+template<typename T>
+class F_class
+{
+public:
+	void run(MyClass<T> a){
+		cout << a.t << endl;
+	}
+};
+
+int main(){
+	MyClass<int> mc(2);
+	friend_run(mc);
+	f_run(mc);
+	F_class<int> fc;
+	fc.run(mc);
+	cin.get();
+	return  0;
+}
